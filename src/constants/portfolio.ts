42eac53b592b355246cb11e275c8ae93cb0a78eb
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-02 17:42:19 +0700

    update

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -2,56 +2,12 @@ import type { Portfolio } from "@/types";
 
 export const portfolioList: Portfolio[] = [
   {
-    title: "My Personal Portfolio",
-    imageThumbnail: "/assets/images/project-01.png",
-    description:
-      "This is a personal branding project that I created using Next JS, Tailwind CSS, and Framer Motion. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://ibrahimelgibran.com/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "Next JS",
-        logo: "/assets/svg/next js.svg",
-      },
-      {
-        tech: "Tailwind",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "Framer Motion",
-        logo: "/assets/svg/framer motion.svg",
-      },
-    ],
-  },
-  {
-    title: "Personal Profile BUYR",
-    imageThumbnail: "/assets/images/project-02.png",
-    description:
-      "This is a startup branding project that I created using React JS, Tailwind CSS, and Vite. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://projectprofilebuyr.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "React JS",
-        logo: "/assets/svg/react.svg",
-      },
-      {
-        tech: "Tailwind",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "Vite",
-        logo: "/assets/svg/vite.svg",
-      },
-    ],
-  },
-  {
     title: "Website Profile PPDB",
     imageThumbnail: "/assets/images/project-06.png",
     description:
-      "Coming Soon! This is a Website Profile PPDB 2024-2025 project that I created using Codeigniter 3, Bootstrap, MySQL. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
+      "This is a Website Profile PPDB 2024-2025 project that I created using Codeigniter 3, Bootstrap, MySQL. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
     livePreviewURL: "https://ppdbv2.smksmuhammadiyahgamping.sch.id/",
-    repositoryURL: "#",
+    repositoryURL: "https://github.com/ibrahimelgibran/ci-Eskamuga",
     techStack: [
       {
         tech: "PHP",
@@ -68,52 +24,12 @@ export const portfolioList: Portfolio[] = [
     ],
   },
   {
-    title: "IegCode Busniess",
-    imageThumbnail: "/assets/images/project-18.png",
-    description:
-      "This is a WebApp Busniess Tech Stack Next Js, Tailwind CSS. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://www.iegcodestore.my.id/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "Next JS",
-        logo: "/assets/svg/next js.svg",
-      },
-      {
-        tech: "Tailwind",
-        logo: "/assets/svg/tailwind.svg",
-      },
-    ],
-  },
-  {
-    title: "Management Cource Data",
-    imageThumbnail: "/assets/images/project-16.png",
-    description:
-      "This is a Management of Cource Student Data With System Dashbaord, CRUD and Tech Stack PHP Versi 8, MYSQL, CSS, BOOTSTRAP 5. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "#",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "Bootstrap",
-        logo: "/assets/svg/bootstrap.svg",
-      },
-      {
-        tech: "MYSQL",
-        logo: "/assets/svg/mysql.svg",
-      },
-    ],
-  },
-  {
     title: "CMS Eskamuga",
     imageThumbnail: "/assets/images/project-09.png",
     description:
       "This is a CMS ESKAMUGA project that I created using PHP, Codeignither 4 and MySQL. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
     livePreviewURL: "https://smksmuhammadiyahgamping.sch.id/",
-    repositoryURL: "#",
+    repositoryURL: "https://github.com/ibrahimelgibran/Final-Project-CMS",
     techStack: [
       {
         tech: "PHP",
@@ -130,66 +46,12 @@ export const portfolioList: Portfolio[] = [
     ],
   },
   {
-    title: "Youtube Clone",
-    imageThumbnail: "/assets/images/project-11.png",
-    description:
-      "This is a IegcodeTube is a youtube's clone website that i've created using React JS, Material UI and RapidAPI as data API for learning purposes. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegcodetube.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "React JS",
-        logo: "/assets/svg/react.svg",
-      },
-      {
-        tech: "Material UI",
-        logo: "/assets/svg/material ui.svg",
-      },
-    ],
-  },
-  {
-    title: "Github Clone",
-    imageThumbnail: "/assets/images/project-17.png",
-    description:
-      "This is a Implement an API fetch of your GitHub portfolio, show a page with a list of all your repositories on GitHub, and show another page linking to each repo using nested routes while using all the necessary tools in react. Implement the proper SEO, Error Boundary (show a page to test the error boundary) and 404 pages. Good UI and Designs are important. Tech Stack ReactJS and Vanila CSS. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegcodegithub.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "ReactJS",
-        logo: "/assets/svg/react.svg",
-      },
-      {
-        tech: "Vanila CSS",
-        logo: "/assets/svg/css.svg",
-      },
-    ],
-  },
-  {
-    title: "My Personal Profile",
-    imageThumbnail: "/assets/images/project-03.png",
-    description:
-      "This is a personal branding project that I created using Tailwind CSS and Bootstap. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://ibrahimelgibran.github.io/PersonalTemplate/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "TailwindS",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "Bootstrap",
-        logo: "/assets/svg/bootstrap.svg",
-      },
-    ],
-  },
-  {
     title: "CBT Online",
     imageThumbnail: "/assets/images/project-15.png",
     description:
       "This is a CBT online SMK MUHAMMADIYAH GAMPING for students ESKAMUGA with own fiture complite, i created using tech stack Codeigniter 4, CSS, Bootstrap, JavaScript, MYSQL. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
     livePreviewURL: "https://ujianonline.smksmuhammadiyahgamping.sch.id/",
-    repositoryURL: "#",
+    repositoryURL: "https://github.com/ibrahimelgibran/IegCBT",
     techStack: [
       {
         tech: "PHP",
@@ -211,7 +73,7 @@ export const portfolioList: Portfolio[] = [
     description:
       "This is a personal profile food stalls with various types of soto in the Yogyakarta area that I created using Sass, Bootstap and JavaScript. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
     livePreviewURL: "https://sotoijomonjali.com/",
-    repositoryURL: "#",
+    repositoryURL: "https://github.com/ibrahimelgibran/soim",
     techStack: [
       {
         tech: "Sass",
@@ -228,63 +90,19 @@ export const portfolioList: Portfolio[] = [
     ],
   },
   {
-    title: "Berbrandstuff",
-    imageThumbnail: "/assets/images/project-07.png",
+    title: "IegCode Busniess",
+    imageThumbnail: "/assets/images/project-18.png",
     description:
-      "This is a personal profile website Berbrandstuff project that I created using HTML, CSS, Bootstap, Javascript. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://berbrandstuff.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "HTML5",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "Bootstrap",
-        logo: "/assets/svg/bootstrap.svg",
-      },
-      {
-        tech: "Javascript",
-        logo: "/assets/svg/javascript.svg",
-      },
-    ],
-  },
-  {
-    title: "Undangan WeddingV2",
-    imageThumbnail: "/assets/images/project-14.png",
-    description:
-      "This is a Web Wedding version 2 and own fiture mp3, O'clock, Maps, Save date, gallery photo, animation loading dll. that I created using HTML, CSS, Bootstap, Javascript. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegcodeweddingv2.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "HTML5",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "Bootstrap",
-        logo: "/assets/svg/bootstrap.svg",
-      },
-      {
-        tech: "Javascript",
-        logo: "/assets/svg/javascript.svg",
-      },
-    ],
-  },
-  {
-    title: "AnimeList API Jikan",
-    imageThumbnail: "/assets/images/project-04.png",
-    description:
-      "This is a Anime List project that I created using React JS And Tailwind CSS connect to API Jikan. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://anime-list-lyart.vercel.app/",
-    repositoryURL: "#",
+      "This is a WebApp Busniess Tech Stack Next Js, Tailwind CSS. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
+    livePreviewURL: "https://www.iegcodestore.my.id/",
+    repositoryURL: "https://github.com/ibrahimelgibran/iegcodestore",
     techStack: [
       {
         tech: "Next JS",
-        logo: "/assets/svg/react.svg",
+        logo: "/assets/svg/next js.svg",
       },
       {
-        tech: "Tailwind CSS",
+        tech: "Tailwind",
         logo: "/assets/svg/tailwind.svg",
       },
     ],
@@ -295,7 +113,7 @@ export const portfolioList: Portfolio[] = [
     description:
       "This is a Busniess WebApp Profil GMTcorp.id project that I created using HTML, CSS And JavaScript. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
     livePreviewURL: "https://gemilangmultiteknologi.my.id/",
-    repositoryURL: "#",
+    repositoryURL: "https://github.com/ibrahimelgibran/GemilangMultiTeknologi",
     techStack: [
       {
         tech: "HTML",
@@ -303,112 +121,6 @@ export const portfolioList: Portfolio[] = [
       },
       {
         tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-    ],
-  },
-  {
-    title: "Eskamuga BKK & E-Learing",
-    imageThumbnail: "/assets/images/project-10.png",
-    description:
-      "This is a Web Profile Add Fiture BKK, E-Learing and Page Gallery from CMS ESKAMUGA Tech Stack HTML5, CSS3, BOOTSTRAP, JAVASCRIPT. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://eskamuga.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "HTML5",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "Vanila CSS",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-    ],
-  },
-  {
-    title: "Undangan WeddingV1",
-    imageThumbnail: "/assets/images/project-13.png",
-    description:
-      "This is a Web Wedding digital and many fiture complite like mp3, maps, photo, O'clock, chat live congratulations. Tech Stack HTML5, CSS3, BOOTSTRAP, JAVASCRIPT. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegcodewedding.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "HTML5",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "Vanila CSS",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-    ],
-  },
-  {
-    title: "Resume Profil",
-    imageThumbnail: "/assets/images/project-20.png",
-    description:
-      "This is a Web Resume Profil Portfolio. Tech Stack Valina CSS, JavaScript, NodeJs. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegresumeportfolio.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "NodeJs",
-        logo: "/assets/svg/node js.svg",
-      },
-      {
-        tech: "Vanila CSS",
-        logo: "/assets/svg/css.svg",
-      },
-    ],
-  },
-  {
-    title: "Movie Api TMDB",
-    imageThumbnail: "/assets/images/project-05.png",
-    description:
-      "This is a Testing Movie API TMDB project that I created using React JS. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://branmovietmbdapi.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "Recat JS",
-        logo: "/assets/svg/react.svg",
-      },
-      {
-        tech: "Vanila CSS",
-        logo: "/assets/svg/css.svg",
-      },
-    ],
-  },
-  {
-    title: "Link in Bio",
-    imageThumbnail: "/assets/images/project-12.png",
-    description:
-      "This is a Create Link in Bio page from my profile Instagram tech stack HTML, CSS and JavaScript. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "https://iegcodelink.vercel.app/",
-    repositoryURL: "#",
-    techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "Vanila CSS",
         logo: "/assets/svg/css.svg",
       },
       {
